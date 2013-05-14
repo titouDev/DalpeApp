60277f913d504846ebbd51fb71d978b7d2732862
titouDev <cbonnaffoux@me.com>
2013-05-13 20:13:00 -0400

    changing sqlconnect  pour publication sur titoudev

--- php/mysql_connect.php
+++ php/mysql_connect.php
@@ -2,7 +2,7 @@
 
 	function connectToDbMySql()
 	{
-	    $_db = new mysqli('localhost', 'dalpe' ,'simon', 'dalpe_construction');
+	    $_db = new mysqli('localhost', 'titoudev_simon' ,'Ucantd0it', 'titoudev_dalpe');
 	
 	    if ($_db->connect_error) {
 	        die('Connection Error (' . $_db->connect_errno . ') ' . $_db->connect_error);
