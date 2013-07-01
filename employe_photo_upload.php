d62ee4fe4253660105a0ee97428f5991dcc4e5f5
titouDev <cbonnaffoux@me.com>
2013-07-01 10:30:11 -0400

    Path doc upload correction

--- employe_photo_upload.php
+++ employe_photo_upload.php
@@ -9,16 +9,11 @@ if(isset($_FILES)){
 	$file_type = $_FILES['photoImport']['type'];
 
     //echo ($file_tmp.", ".$file_name.", ".$file_size);
-	fb($file_tmp);
-    if(is_uploaded_file($file_tmp)) {
-    	fb(1);
+	if(is_uploaded_file($file_tmp)) {
     	if (isset($_POST['employeId']))
     	{
-	    	fb(2);
-	    	$destPath = "documents_importes/employes/$file_name";
-	    	
+	    	$destPath = "./documents_importes/employes/$file_name";
 	    	if(move_uploaded_file($file_tmp, $destPath)){
-	            fb(3);
 	            //On sauve le document dans la DB
 				$_db = connectToDbMySql();
 				$query = 'UPDATE employes SET
@@ -34,12 +29,10 @@ if(isset($_FILES)){
 						    }
 				//On creer les liens dans la DB
 	        } else {
-	            fb(4);
 	            echo '{success: false}';
 	        }
 		}    
     }  else{
-    	fb(5);
     	echo '{success: false}';
     }
 }
