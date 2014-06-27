1e361bd63d9d110252e4953206b3f8d7cb5c84d5
titouDev <titou@Cedrics-MacBook-Pro.local>
2014-06-26 22:04:44 -0400

    many to many fonctionnels !!
    *gros menage de code
    *factorization
    *removing sqlsoup

--- Flask/Models/baseAlchemy.py
+++ Flask/Models/baseAlchemy.py
@@ -1,7 +1,6 @@
 
 
 import sqlalchemy
-import sqlsoup
 import json
 import datetime
 import logging
@@ -28,9 +27,7 @@ engine = create_engine(dbSqLite, echo=False, case_sensitive=False)
 
 Base.metadata.create_all(engine)
 
-#db = sqlsoup.SQLSoup(engine)
 Session = sessionmaker(bind=engine)
-#session = Session()
 
 from contextlib import contextmanager
 
@@ -61,112 +58,30 @@ def get(modelName, **kwargs):
 def getColumns(model):
     return [c.name for c in model.__table__.columns]
 
-def get_old(modelName, id=False, **kwargs):
-    filter = kwargs.get('filter')
-    if modelName == "sousTraitants":
-        specialiteId = kwargs.get('specialiteId')
-        if specialiteId:
-            sousTraitantIds = [link['sousTraitantId'] for link in get_default('soustraitants_link_specialites', **{'specialiteId':specialiteId[0]})]
-            return get_default(modelName, **{'id':sousTraitantIds})
-    if filter is not None:
-        if modelName == "mails":
-            joinTable = "soustraitants_link_mails"
-            dictFilter = dict([('%s_%s' % (joinTable, i['property']), i['value']) for i in json.loads(filter[0])])
-            return get_default(modelName,join=joinTable, **dictFilter)
-        
-    return get_default(modelName, id=id)
-
-def get_default(modelName,id=False, join=False, **filters):
-    if id:
-        filters["id"]=id
-    db = sqlsoup.SQLSoup(engine)
-    table = db.entity(modelName)
-    tableFields = [i for i in table._sa_class_manager]
-    if join:
-        labeled_join = db.with_labels(db.entity(join))
-        table = db.join(table, labeled_join)
-    data = [] 
-    for i in table.filter_by(**filters).all():
-        dataObject = dict([(f, str(getattr(i, f))) for f in tableFields])
-        data.append(dataObject)
-    return data
-
-
 def create(modelName, **kwargs):
     with session_scope() as session:
-        if modelName == 'sousTraitants':
-            record = createSoustraitant(session, **kwargs)
-        else:
-            record = createModel(session, modelName, **kwargs)
+        record = createModel(session, modelName, **kwargs)
         session.commit()
         return record.toJson()
-
-def createSoustraitant(session, **kwargs):
-    specialites = kwargs.pop('specialites', False)
-    st = createModel(session, 'sousTraitants', uniqueKey='name', **kwargs)
-
-    if specialites:
-        appendSpecialitesToSoustraitants(session, st, specialites)
-    return st
-
-def updateSoustraitant(session, **kwargs):
-    specialites = kwargs.pop('specialites', False)
-    st = updateModel(session, 'sousTraitants', **kwargs)
-    if specialites:
-        appendSpecialitesToSoustraitants(session, st, specialites)
-    return st
-
-def appendSpecialitesToSoustraitants(session, st, specialites):
-    st.specialites = [createSpecialite(session, name=s) for s in specialites]
-
-def createSpecialite(session, **kwargs):
-    return createModel(session, 'specialites', uniqueKey='name',returnExisting=True, **kwargs)
 
 def update(modelName, **kwargs):
     with session_scope() as session:
-        if modelName == 'sousTraitants':
-            record = updateSoustraitant(session, **kwargs)
-        else:
-            record = updateModel(session, modelName, **kwargs)
+        record = updateModel(session, modelName, **kwargs)
         session.commit()
         return record.toJson()
 
-def createModel(session, modelName, uniqueKey=None,returnExisting=False, onDuplicateUpdate=False, **kwargs):
+def createModel(session, modelName, **kwargs):
     model=get_class(modelName)
-    if uniqueKey is not None:
-        query = session.query(model).filter(getattr(model, uniqueKey) == kwargs[uniqueKey])
-        record = query.first()
-        if not record:
-            record = model(**kwargs)
-            session.add(record)
-        elif returnExisting:
-            pass
-        elif onDuplicateUpdate:
-            query.update(kwargs)
-        else:
-            raise ValueError('%s with %s %s already exist' % (modelName, uniqueKey, kwargs[uniqueKey]))
-        return record
-    else:
-        record = model(**kwargs)
-        session.add(record)
-        return record
-
+    return model().createRecord(session, **kwargs)
 
 def updateModel(session, modelName, **kwargs):
     model = get_class(modelName)
-    query = session.query(model).filter(model.id==kwargs["id"])
-    query.update(kwargs)
-    return query.first()
-
-
-
+    return model().updateRecord(session, **kwargs)
 
 if __name__ == '__main__':
     with session_scope() as session:
-        
-        s = session.query(sousTraitants).first()
-        s.documents.append(documents(name='tutu'))
+        print createModel(session, 'sousTraitants', name="tutu", specialites=["Beton"], documents=["Pdf"]).toJson()
         session.commit()
-        #print session.query(sousTraitants).join(Soustraitants_link_documents, documents).filter(documents.name.in_(['tutu'])).all()
-        print session.query(sousTraitants).filter(sousTraitants.documents.any(name='tutu')).all()
+
+
 
--- Flask/Models/classSqlAlchemy.py
+++ Flask/Models/classSqlAlchemy.py
@@ -12,6 +12,9 @@ from sqlalchemy.orm import (relationship,
 
 Base = declarative_base()
 
+get_class = lambda x: globals()[x]
+
+
 def serialize(model):
     """Transforms a model into a dictionary which can be dumped to JSON."""
     # first we get the names of all the columns on your model
@@ -19,11 +22,65 @@ def serialize(model):
     # then we return their values in a dict
     return dict((c, getattr(model, c)) for c in columns)
 
-class AddonsBase():
-    def toJson(self):
-        return serialize(self)
+def getColumns(model):
+    return [c.name for c in model.__table__.columns]
 
+
+class AddonsBase():
+    subModels = []
+    uniqueKey=None
+    onDuplicateUpdate=False
+    returnIfExists=False
+    def toJson(self):
+        jsonRecord = serialize(self)
+        for m in self.subModels:
+            records = [serialize(record) for record in getattr(self, m)]
+            jsonRecord[m] = records
+        return jsonRecord
     
+    
+    def updateRecord(self, session, **kwargs):
+        model = self.__class__
+        otherKwargs = kwargs.viewkeys() - set(getColumns(model))
+        rejectedKwargs = dict((k, kwargs.pop(k)) for k in otherKwargs)
+        query = session.query(model).filter_by(id=kwargs["id"])
+        query.update(kwargs)
+        record = query.first()
+        for k, v in rejectedKwargs.items():
+            record._appendSubModels(session, k, v)
+        return record
+
+    def createRecord(self, session, **kwargs):
+        model=self.__class__
+
+        otherKwargs = kwargs.viewkeys() - set(getColumns(model))
+        rejectedKwargs = dict((k, kwargs.pop(k)) for k in otherKwargs)
+        if self.uniqueKey is not None:
+            query = session.query(model).filter(getattr(model, self.uniqueKey) == kwargs[self.uniqueKey])
+            record = query.first()
+            if not record:
+                record = model(**kwargs)
+                session.add(record)
+            elif self.returnIfExists:
+                pass
+            elif self.onDuplicateUpdate:
+                query.update(kwargs)
+            else:
+                raise ValueError('%s with %s %s already exist' % (modelName, self.uniqueKey, kwargs[self.uniqueKey]))
+            for k, v in rejectedKwargs.items():
+                record._appendSubModels(session, k, v)
+            return record
+        else:
+            record = model(**kwargs)
+            session.add(record)
+            return record
+
+    def _appendSubModels(self, session, modelName, records):
+        print modelName, records
+        model = get_class(modelName)
+        [getattr(self, modelName).append(model().createRecord(session, name=r)) for r in records]
+            
+
 
 class chantiers(Base, AddonsBase):
     __tablename__ = 'chantiers'
@@ -148,13 +205,10 @@ class sousTraitants(Base, AddonsBase):
                     )
     documents = relationship("documents",
                     secondary=lambda: Soustraitants_link_documents,
-                    backref="soustraitants")
-    def toJson(self):
-        
-        specialites = [serialize(s) for s in self.specialites]
-        dictRecord = serialize(self)
-        dictRecord["specialites"] = specialites
-        return dictRecord
+                    backref="sousTraitants")
+    subModels = ['specialites', 'documents']
+    uniqueKey='name'
+    onDuplicateUpdate=True
 
 class Soustraitants_link_mails(Base, AddonsBase):   
     __tablename__ = 'soustraitants_link_mails'  
@@ -172,6 +226,9 @@ class specialites(Base, AddonsBase):
     id =                    Column(Integer, primary_key=True)
     name =                  Column(String, nullable=False, unique=True)
     
+    uniqueKey='name'
+    returnIfExists=True
+
     def __repr__(self):
         return "<Specialites(name='%s')>" % (self.name)
 Soustraitants_link_documents = Table('soustraitants_link_documents', Base.metadata,   
