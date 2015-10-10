17cd5b4560a5c3b2ea90961158e1b4e0126848cb
Cedric Bonnaffoux <titou@Jacinthes-MacBook-Pro.local>
2015-10-10 14:34:41 -0400

    architect 3.2 ext js 4.2 et cmd working

---
+++ Flask/static/build/temp/development/dalpeApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/titou/Documents/GIT/DalpeApp/Flask/static/build/.sass-cache'
---
+++ Flask/static/build/temp/development/dalpeApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,4 @@
+// @tag full-page
+// @require /Users/titou/Documents/GIT/DalpeApp/Flask/static/js/login_infos.js
+// @require /Users/titou/Documents/GIT/DalpeApp/Flask/static/plugins/rsvp.js
+// @require /Users/titou/Documents/GIT/DalpeApp/Flask/static/app.js
